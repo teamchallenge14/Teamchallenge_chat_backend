8aea2222af5fa940639a1c10ed0fead356369de3
lunaris <LunarisMax@proton.me>
2026-01-12 16:38:52 +0200

    fix: fix miss config

--- prisma/prisma.service.ts
+++ prisma/prisma.service.ts
@@ -1,16 +1,14 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
+import { dbConfig } from 'src/config';
 
 @Injectable()
-export class PrismaService
-  extends PrismaClient
-  implements OnModuleInit, OnModuleDestroy
-{
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   constructor() {
     super({
       adapter: new PrismaPg({
-        connectionString: process.env.POSTGRES_DATABASE_URL!,
+        connectionString: dbConfig.postgres.url,
       }),
     });
   }
