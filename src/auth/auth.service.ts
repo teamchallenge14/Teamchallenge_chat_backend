e0dd275c7a8924a384d823bbe09f1d979cc43d9e
lunaris <LunarisMax@proton.me>
2026-01-20 18:55:09 +0200

    fix: off social auth cookie secure

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -152,15 +152,15 @@ export class AuthService {
 
     res.cookie('access_token', accessToken, {
       httpOnly: true,
-      secure: true,
-      sameSite: 'none',
+      secure: false,
+      sameSite: 'strict',
       maxAge: 15 * 60 * 1000,
     });
 
     res.cookie('refresh_token', refreshToken, {
       httpOnly: true,
-      secure: true,
-      sameSite: 'none',
+      secure: false,
+      sameSite: 'strict',
       maxAge: 30 * 24 * 60 * 60 * 1000,
     });
   }
