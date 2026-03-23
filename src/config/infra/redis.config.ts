8b186aad5ed741ff9a918b6c10abd7302ce4aab6
Vladislav Virchenko <virchenko.vlad.2011@icloud.com>
2026-03-23 16:31:25 +0200

    feat: wire redis

--- src/config/infra/redis.config.ts
+++ src/config/infra/redis.config.ts
@@ -1,9 +1,10 @@
 import { get } from 'env-var';
 
 export const redisConfig = {
-  host: get('REDIS_HOST').required().asString(),
-
-  port: get('REDIS_PORT').required().asPortNumber(),
-
+  url: get('REDIS_URL').default('').asString() || undefined,
+  host: get('REDIS_HOST').default('localhost').asString(),
+  port: get('REDIS_PORT').default('6379').asPortNumber(),
+  username: get('REDIS_USERNAME').default('').asString() || undefined,
   password: get('REDIS_PASSWORD').default('').asString() || undefined,
+  tls: get('REDIS_TLS').default('false').asBool(),
 } as const;
